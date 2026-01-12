119279a2d610616d6a6cb82fc774ec535955c5cc
Galassios <132308621+Galassios@users.noreply.github.com>
2026-01-12 18:06:53 -0300

    Create analise_salarios.py

---
+++ projeto-01-analise-salarial/python/analise_salarios.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.read_csv('dados/dados_pessoas.csv')
+
+resultado = (
+    df
+    .drop_duplicates()
+    .groupby('area')
+    .agg(
+        media=('salario', 'mean'),
+        quant=('nome', 'count')
+    )
+    .round(2)
+    .sort_values(by=['media', 'quant'], ascending=False)
+)
+
+print(resultado)
