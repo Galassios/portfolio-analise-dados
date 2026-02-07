f28ee86e1d97eed87bd9e6e4502fc193472b022a
Galassios <132308621+Galassios@users.noreply.github.com>
2026-02-07 19:15:00 -0300

    Update analise_salarial.py

--- projeto-04-analise-salarial-visualizacao/scripts/analise_salarial.py
+++ projeto-04-analise-salarial-visualizacao/scripts/analise_salarial.py
@@ -1,13 +1,22 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("dados/pessoas.csv")
+pessoas = pd.read_csv("dados/pessoas.csv")
+areas = pd.read_csv("dados/areas.csv")
+
+df = pessoas.merge(
+    areas,
+    left_on="area_id",
+    right_on="id_area",
+    how="left"
+)
 
 media_geral = df["salario"].mean()
 print(f"Média geral de salário: {media_geral:.2f}")
 
 resumo_area = (
     df
-    .groupby("area_id")
+    .groupby("nome_area")
     .agg(
         media_salario=("salario", "mean"),
         total_pessoas=("nome", "count")
@@ -34,4 +43,20 @@ resumo_acima_media = (
 )
 
 print(resumo_acima_media)
+
+ax = resumo_area["media_salario"].plot(kind="bar")
+
+plt.axhline(
+    y=media_geral,
+    linestyle="--",
+    linewidth=2,
+    label="Média geral"
+)
+
+plt.title("Salário médio por área")
+plt.ylabel("Salário médio")
+plt.xlabel("Área")
+plt.legend()
+plt.tight_layout()
+plt.show()
 
